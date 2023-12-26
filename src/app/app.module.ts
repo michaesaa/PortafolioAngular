b1837f7af4b9711d0d4f5d39f72760df847ed652
michael <michaelsaams20@gmail.com>
2023-12-26 09:26:11 -0500

    sincronizacion 26-12-2023  9:26

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BodyComponent } from './body/body.component';
+import { AcercaComponent } from './acerca/acerca.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { TrabajosComponent } from './trabajos/trabajos.component';
+import { TableComponent } from './table/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    BodyComponent,
+    AcercaComponent,
+    ServiciosComponent,
+    ContactoComponent,
+    TrabajosComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
